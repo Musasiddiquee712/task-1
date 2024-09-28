3462bfd2504f9c0b75760cd5af3248838fb2d27b
Musasiddiquee712 <musa78906u@gmail.com>
2024-09-28 06:41:04 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let num1 = prompt("Name") 
+
+let num2 = prompt("gender") 
+
+let num3 = prompt("Password") 
+
+let num4 = prompt("con-password") 
+
+ Array = [num1,num2,num3,num4]
+ 
+console.log(Array);
+
